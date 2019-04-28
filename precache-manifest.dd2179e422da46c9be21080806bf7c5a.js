8eb5126ae06fb8f6ba9513b45aa61ab767105354
Fernando Souza <nandosouzafilho@gmail.com>
2019-04-28 13:59:06 +0200

    Updates

---
+++ precache-manifest.dd2179e422da46c9be21080806bf7c5a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba9a2b5502025e8d68c77b210a4e27e8",
+    "url": "/react-tic-tac-toe/index.html"
+  },
+  {
+    "revision": "aaf326d45efe0ab7bd08",
+    "url": "/react-tic-tac-toe/static/css/main.c1a7ed4c.chunk.css"
+  },
+  {
+    "revision": "83e44c7371c7b0a0f5c5",
+    "url": "/react-tic-tac-toe/static/js/2.342a91f2.chunk.js"
+  },
+  {
+    "revision": "aaf326d45efe0ab7bd08",
+    "url": "/react-tic-tac-toe/static/js/main.7a4e367a.chunk.js"
+  },
+  {
+    "revision": "140d55aca7c0751087c1",
+    "url": "/react-tic-tac-toe/static/js/runtime~main.a5f725ed.js"
+  },
+  {
+    "revision": "a0bd5259b3d1ef0c32aedd4d86ea3ce5",
+    "url": "/react-tic-tac-toe/static/media/circle.a0bd5259.svg"
+  },
+  {
+    "revision": "9de3d925689cf5e4cbb087fd0525b32f",
+    "url": "/react-tic-tac-toe/static/media/x.9de3d925.svg"
+  }
+]);
